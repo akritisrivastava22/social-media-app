a3e513f1b9c118617b9a72bc4f6f24fc68f06648
Akriti Srivastava <120243099+akritisrivastava22@users.noreply.github.com>
2026-05-26 17:09:50 +0530

    Update QuestionappApplicationTests.java

--- src/test/java/com/example/questionapp/QuestionappApplicationTests.java
+++ src/test/java/com/example/questionapp/QuestionappApplicationTests.java
@@ -1,7 +1,7 @@
 package com.example.questionapp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest; #testing
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class QuestionappApplicationTests {
