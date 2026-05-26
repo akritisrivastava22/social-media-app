dc0f1a1abc12e6e1b99b57c8862e73bba16b7bb0
Akriti Srivastava <120243099+akritisrivastava22@users.noreply.github.com>
2026-05-26 10:17:17 +0530

    Update LikeResponse.java

--- src/main/java/com/example/questionapp/responses/LikeResponse.java
+++ src/main/java/com/example/questionapp/responses/LikeResponse.java
@@ -4,7 +4,7 @@ import com.example.questionapp.entities.Like;
 import lombok.Data;
 
 @Data
-public class LikeResponse {  //user ve postun hepsini dönmek yerine sadece Id lerini dönücez bu yüzden LikeResponse oluşturduk.
+public class LikeResponse {  
 
     private Long id;
     private Long userId;
